00fff7dad9d475678742041a061210218c0ab47f
Danny <dfan1028@gmail.com>
2024-05-03 19:18:09 -0700

    Compare if string are valid

--- lib/omniauth/strategies/oauth2.rb
+++ lib/omniauth/strategies/oauth2.rb
@@ -186,6 +186,7 @@ module OmniAuth
 
        # constant-time comparison algorithm to prevent timing attacks
        def secure_compare(string_a, string_b)
+        return false unless string_a && string_b
         return false unless string_a.bytesize == string_b.bytesize
 
         l = string_a.unpack "C#{string_a.bytesize}"
